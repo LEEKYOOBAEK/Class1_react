b0d0a9d03620032844d33fd8f8923d075eb74ec9
LeeKyooBaek <charisma1124@gmail.com>
2021-07-31 03:17:59 +0900

    mode 변경 및 control.js / create/update/delete button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
 import Content from './components/Content';
-import Subject from './components/Subject.js';
+import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 
@@ -63,6 +64,11 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
